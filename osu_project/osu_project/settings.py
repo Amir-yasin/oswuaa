40c1a2d2069a7e0ce6a81f867841cae199c52c1c
Amir Yasin <amiryasin3262@gmail.com>
2024-10-24 12:16:31 +0300

    Changes to be committed: 	modified:   osu_project/osu_project/settings.py

--- osu_project/osu_project/settings.py
+++ osu_project/osu_project/settings.py
@@ -131,9 +131,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_DIRS = [os.path.join(BASE_DIR,'osu_project/osu_project/static')]
-STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'osu_project', 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 LOGIN_URL = 'logins'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
