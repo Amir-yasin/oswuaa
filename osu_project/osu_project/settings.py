824b0cc0517b3a4e054b9ecabe1f4b77bfbed4c8
Amir Yasin <amiryasin3262@gmail.com>
2024-10-29 00:05:43 +0300

    Changes to be committed: 	modified:   osu_project/osu_project/settings.py

--- osu_project/osu_project/settings.py
+++ osu_project/osu_project/settings.py
@@ -23,7 +23,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get("SECRET_KEY")
 #django-insecure-2&#m31@u-5nu^^_*vnia3^2vokv(j-qz%g-rb1x)-bal%ikd6n
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(',')
