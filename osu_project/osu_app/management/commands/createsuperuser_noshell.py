99785d6e4c41fec29def3dcc46220379e703ac59
Amir Yasin <amiryasin3262@gmail.com>
2024-10-29 12:17:12 +0300

    Changes to be committed: 	modified:   osu_project/osu_app/management/commands/createsuperuser_noshell.py

--- osu_project/osu_app/management/commands/createsuperuser_noshell.py
+++ osu_project/osu_app/management/commands/createsuperuser_noshell.py
@@ -1,17 +1,18 @@
+
+
 from django.core.management.base import BaseCommand
-from django.contrib.auth import get_user_model
+from django.contrib.auth.models import User
 
 class Command(BaseCommand):
-    help = 'Create a superuser without shell access'
+    help = 'Create a superuser'
 
-    def handle(self, *args, **options):
-        User = get_user_model()
-        if not User.objects.filter(username="admin").exists():
-            User.objects.create_superuser(
-                username="amiryasin",
-                email="amiryasin3262@gmail.com",
-                password="a1b2c3d4e5"
-            )
-            self.stdout.write(self.style.SUCCESS("Superuser created successfully."))
+    def handle(self, *args, **kwargs):
+        username="amiryasin",
+        email="amiryasin3262@gmail.com",
+        password="a1b2c3d4e5"
+
+        if not User.objects.filter(username=username).exists():
+            User.objects.create_superuser(username=username, email=email, password=password)
+            self.stdout.write(self.style.SUCCESS('Superuser created successfully.'))
         else:
-            self.stdout.write(self.style.WARNING("Superuser already exists."))
+            self.stdout.write(self.style.WARNING('Superuser already exists.'))
