1d16cb050ceaabbfed1b86cef1e765237c16f4ad
Amir Yasin <amiryasin3262@gmail.com>
2024-10-29 13:08:51 +0300

    Changes to be committed: 	modified:   osu_project/osu_app/management/commands/create_superuser.py

--- osu_project/osu_app/management/commands/create_superuser.py
+++ osu_project/osu_app/management/commands/create_superuser.py
@@ -1,15 +1,16 @@
 # osu_app/management/commands/create_superuser.py
 
 from django.core.management.base import BaseCommand
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model  # Import to get the user model
 
 class Command(BaseCommand):
     help = 'Create a superuser'
 
     def handle(self, *args, **kwargs):
-        username = "amiryasin"  # Remove the comma
-        email = "amiryasin3262@gmail.com"  # Remove the comma
-        password = "a1b2c3d4e5"  # Remove the comma
+        User = get_user_model()  # Get the custom user model
+        username = "amiryasin"
+        email = "amiryasin3262@gmail.com"
+        password = "a1b2c3d4e5"
 
         if not User.objects.filter(username=username).exists():
             User.objects.create_superuser(username=username, email=email, password=password)
